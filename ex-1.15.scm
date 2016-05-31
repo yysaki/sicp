50ca511705d949cd9548661471bc3db753d6cea2
yysaki <yysaki@mail.yysaki.com>
2016-05-31 23:47:35 +0900

    Answer ex-1.15, except calculating order of growth

---
+++ ex-1.15.scm
@@ -0,0 +1,14 @@
+(define (cube x) (* x x x))
+
+(define (p x) (- (* 3 x) (* 4 (cube x))))
+(define (p-spy x)
+  (print x)
+  (p x))
+
+(define (sine angle)
+   (if (not (> (abs angle) 0.1))
+       angle
+       (p-spy (sine (/ angle 3.0)))))
+
+(sine 12.15) ; 5回呼び出される
+
