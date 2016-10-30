4f29c7a0a7acd6a551547269f59b12ccc9863fcf
yysaki <yysaki@mail.yysaki.com>
2016-10-30 18:09:54 +0900

    Fix 2.50 - 2.51

--- ex-2.50.scm
+++ ex-2.50.scm
@@ -11,7 +11,7 @@
 
 (define (flip-horiz painter)
   (transform-painter painter
-                     (make-vect 0 1)
+                     (make-vect 1 0)
                      (make-vect 0 0)
                      (make-vect 1 1)))
 
@@ -26,3 +26,17 @@
                      (make-vect 0 1)
                      (make-vect 0 0)
                      (make-vect 1 1)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (rotate90 painter)
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 0.0)))
+
+(define (rotate180 painter)
+  (rotate90 (rotate90 painter)))
+
+(define (rotate270 painter)
+  (rotate180 (rotate90 painter)))
--- ex-2.51.scm
+++ ex-2.51.scm
@@ -15,3 +15,26 @@
       (lambda (frame)
         (paint-bottom frame)
         (paint-top frame)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (beside painter1 painter2)
+  (let ((split-point (make-vect 0.5 0.0)))
+    (let ((paint-left
+           (transform-painter painter1
+                              (make-vect 0.0 0.0)
+                              split-point
+                              (make-vect 0.0 1.0)))
+          (paint-right
+           (transform-painter painter2
+                              split-point
+                              (make-vect 1.0 0.0)
+                              (make-vect 0.5 1.0))))
+      (lambda (frame)
+        (paint-left frame)
+        (paint-right frame)))))
+
+(define (below painter1 painter2)
+  (rotate90
+    (beside (rotate270 painter1)
+            (rotate270 painter2))))
