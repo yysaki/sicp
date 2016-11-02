0eb7dd1c757983bc52c834058fede69e972e9ade
yysaki <yysaki@mail.yysaki.com>
2016-11-02 20:48:13 +0900

    Update equal? to care cons in 2.54

--- ex-2.54.scm
+++ ex-2.54.scm
@@ -2,9 +2,10 @@
   (cond
     ((and (eq? lhs ()) (eq? rhs ())) #t)
     ((and (symbol? lhs) (symbol? rhs))(eq? lhs rhs))
-    ((not (and (list? lhs) (list? rhs))) #f)
+    ((not (and (pair? lhs) (pair? rhs))) #f)
     (else (and (equal? (car lhs) (car rhs))
                (equal? (cdr lhs) (cdr rhs))))))
 
 (print (equal? '(this is a list) '(this is a list)))
 (print (equal? '(this is a list) '(this (is a) list)))
+(print (equal? '(a . b) '(a . b)))
