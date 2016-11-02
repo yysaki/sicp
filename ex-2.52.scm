61780873ca3653bf7cf04fc3970547283f06bdf7
yysaki <yysaki@mail.yysaki.com>
2016-11-02 20:35:13 +0900

    Update square-limit in 2.52

--- ex-2.52.scm
+++ ex-2.52.scm
@@ -47,4 +47,4 @@
 (define (square-limit painter n)
   (let ((combine4 (square-of-four flip-horiz identity
                                   rotate180 flip-vert)))
-    (combine4 (corner-split painter n))))
+    (combine4 (corner-split (flip-vert painter) n))))
