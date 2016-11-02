c0a061f8a1ccd3de0640607ffb57704846430ed5
yysaki <yysaki@mail.yysaki.com>
2016-11-02 20:13:50 +0900

    Fix rotate-180 in 2.50

--- ex-2.50.scm
+++ ex-2.50.scm
@@ -18,8 +18,8 @@
 (define (rotate-180 painter)
   (transform-painter painter
                      (make-vect 1 1)
-                     (make-vect 1 0)
-                     (make-vect 0 1)))
+                     (make-vect 0 1)
+                     (make-vect 1 0)))
 
 (define (rotate-270 painter)
   (transform-painter painter
