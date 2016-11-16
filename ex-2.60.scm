7991ef71a9338fda3c55c6d12f7e603ca8113233
yysaki <yysaki@mail.yysaki.com>
2016-11-16 20:59:06 +0900

    Solve 2.60

---
+++ ex-2.60.scm
@@ -0,0 +1,26 @@
+(define true #t)
+(define false #f)
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+  (cons x set))
+
+(define (union-set set1 set2)
+  (append set1 set2))
+
+(define (i set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+        ((element-of-set? (car set1) set2)
+         (cons (car set1)
+               (intersection-set (cdr set1) set2)))
+        (else (intersection-set (cdr set1) set2))))
+
+(define (intersection-set set1 set2)
+  (append (i set1 set2)
+          (i set2 set1)))
+
+;(print (intersection-set '(1 1 1 2 3 3) '(2 3 4)))
