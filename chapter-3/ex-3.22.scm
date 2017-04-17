3777ddab6f1526c6d526b5697e03e3d40fdcd45c
yysaki <yysaki@yysaki.com>
2017-04-17 22:44:13 +0900

    Solve 3.22

---
+++ chapter-3/ex-3.22.scm
@@ -0,0 +1,42 @@
+(define (make-queue)
+  (let ((front-ptr '())
+        (rear-ptr '()))
+    (define (empty-queue?)
+      (null? front-ptr))
+    (define (front-queue)
+      (if (empty-queue?)
+        (error "FRONT called with an empty queue")
+        (car front-ptr)))
+    (define (insert-queue! item)
+      (let ((new-pair (cons item '())))
+        (cond ((empty-queue?)
+               (set! front-ptr new-pair)
+               (set! rear-ptr new-pair)
+               front-ptr)
+              (else
+                (set! rear-ptr new-pair)
+                (set-cdr! front-ptr new-pair)
+                front-ptr))))
+    (define (delete-queue!)
+        (cond ((empty-queue?)
+               (error "DELETE! called with an empty queue" queue))
+              (else
+                (set! front-ptr (cdr front-ptr))
+                front-ptr)))
+    (define (dispatch m)
+      (cond ((eq? m 'empty-queue?) empty-queue?)
+            ((eq? m 'front-queue) front-queue)
+            ((eq? m 'insert-queue!) insert-queue!)
+            ((eq? m 'delete-queue!) delete-queue!)))
+    dispatch))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define q (make-queue))
+(print ((q 'empty-queue?)))
+
+(print ((q 'insert-queue!) 'a))
+(print ((q 'insert-queue!) 'b))
+
+(print ((q 'delete-queue!)))
+(print ((q 'delete-queue!)))
