e66fd0488fd54daca629ac44ec190a42a186c223
yysaki <yysaki@yysaki.com>
2017-05-09 22:28:53 +0900

    Fix 3.28

--- chapter-3/ex-3.28.scm
+++ chapter-3/ex-3.28.scm
@@ -11,7 +11,7 @@
 
 (define (logical-or s1 s2)
   (cond ((and (= s1 0) (= s2 0)) 0)
-        ((and (= s1 0) (= s2 1)) 0)
+        ((and (= s1 0) (= s2 1)) 1)
         ((and (= s1 1) (= s2 0)) 1)
         ((and (= s1 1) (= s2 1)) 1)
         (else (error "Invalid signal" s1 s2))))
