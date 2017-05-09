66eca182dbb604201de405beef4565377fc2af39
yysaki <yysaki@yysaki.com>
2017-05-09 23:00:57 +0900

    Run 3.28

--- chapter-3/ex-3.28.scm
+++ chapter-3/ex-3.28.scm
@@ -1,3 +1,5 @@
+(load "./3.3.4")
+
 (define (or-gate a1 a2 output)
   (define (or-action-procedure)
     (let ((new-value
@@ -15,3 +17,32 @@
         ((and (= s1 1) (= s2 0)) 1)
         ((and (= s1 1) (= s2 1)) 1)
         (else (error "Invalid signal" s1 s2))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define the-agenda (make-agenda))
+(define inverter-delay 2)
+(define and-gate-delay 3)
+(define or-gate-delay 5)
+
+(define a1 (make-wire))
+(define a2 (make-wire))
+(define output (make-wire))
+
+(or-gate a1 a2 output)
+
+(probe 'a1 a1)
+(probe 'a2 a2)
+(probe 'output output)
+(propagate)
+
+(define (test v1 v2)
+  (set-signal! a1 v1)
+  (set-signal! a2 v2)
+  (propagate)
+  (newline))
+
+(test 0 0)
+(test 0 1)
+(test 1 0)
+(test 1 1)
