d82be6a5ce11c0c283d782764b7c9e3ea084dcc7
yysaki <yysaki@yysaki.com>
2017-05-10 19:23:05 +0900

    Run 3.29

--- chapter-3/ex-3.29.scm
+++ chapter-3/ex-3.29.scm
@@ -1,3 +1,5 @@
+(load "./3.3.4")
+
 (define (or-gate s1 s2 output)
   (let ((i1 (make-wire))
         (i2 (make-wire))
@@ -14,3 +16,28 @@
 ;
 ; ただし、(inverter s1 i1) と (inverter s2 i2) が同時にadd-action!するならば1 inverter-delay分遅延が少なくなる.
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define the-agenda (make-agenda))
+(define inverter-delay 2)
+(define and-gate-delay 3)
+(define or-gate-delay 5)
+
+(define a1 (make-wire))
+(define a2 (make-wire))
+(define output (make-wire))
+
+(or-gate a1 a2 output)
+
+(probe 'a1 a1)
+(probe 'a2 a2)
+(probe 'output output)
+(propagate)
+
+(define (test v1 v2)
+  (set-signal! a1 v1)
+  (set-signal! a2 v2)
+  (propagate)
+  (newline))
+
+(test 1 1)
