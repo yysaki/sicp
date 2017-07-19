fa5e3dbec13e69e595586680c4444da58ff3599b
yysaki <yysaki@yysaki.com>
2017-07-19 20:37:43 +0900

    Add test for 3.50

--- chapter-3/ex-3.50.scm
+++ chapter-3/ex-3.50.scm
@@ -1,3 +1,5 @@
+(load "./3.5")
+
 (define (stream-map proc . argstreams)
   (if (stream-null? (car argstreams))
       the-empty-stream
@@ -5,3 +7,27 @@
        (apply proc (map stream-car argstreams))
        (apply stream-map
               (cons proc (map stream-cdr argstreams))))))
+
+(display-stream
+    (stream-map
+      list
+      (stream-enumerate-interval 1 10)
+      (stream-map (lambda (x) (* x 3)) (stream-enumerate-interval 1 10))
+      (stream-enumerate-interval 3 12)))
+
+#?=
+(stream-map
+  list
+  (stream-enumerate-interval 1 10)
+  (stream-map (lambda (x) (* x 3)) (stream-enumerate-interval 1 10))
+  (stream-enumerate-interval 3 12))
+
+#?=
+(stream-car
+  (stream-cdr
+    (stream-map
+      list
+      (stream-enumerate-interval 1 10)
+      (stream-map (lambda (x) (* x 3)) (stream-enumerate-interval 1 10))
+      (stream-enumerate-interval 3 12))))
+
