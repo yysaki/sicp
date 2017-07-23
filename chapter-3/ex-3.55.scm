18f9a1726efaaa102f41dcbf5bd946c3b19f3dd1
yysaki <yysaki@yysaki.com>
2017-07-23 18:51:18 +0900

    Fix to solve 3.55

--- chapter-3/ex-3.55.scm
+++ chapter-3/ex-3.55.scm
@@ -1,3 +1,17 @@
+(load "./3.5")
+
 (define (partial-sums s)
   (cons-stream (stream-car s)
-               (add-streams (stream-cdr) (partial-sums s))))
+               (add-streams (stream-cdr s) (partial-sums s))))
+
+(display-stream (stream-take (partial-sums integers) 10))
+; 1
+; 3
+; 6
+; 10
+; 15
+; 21
+; 28
+; 36
+; 45
+; 55
