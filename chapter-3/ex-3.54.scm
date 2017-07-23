b25fa9bfec66147b28a97772d42fef869927979d
yysaki <yysaki@yysaki.com>
2017-07-23 18:42:35 +0900

    Add test for 3.54

--- chapter-3/ex-3.54.scm
+++ chapter-3/ex-3.54.scm
@@ -1,3 +1,5 @@
+(load "./3.5")
+
 ;add-streamsに似て, 二つのストリームの要素毎の積を生じる手続き mul-streamsを定義せよ.
 ; これとintegersストリームを使い, (0から数えて)n番目の要素がn + 1の階乗になる次のストリームの定義を完成せよ:
 
@@ -6,3 +8,5 @@
   (stream-map * s1 s2))
 
 (define factorials (cons-stream 1 (mul-streams integers factorials)))
+
+(display-stream (stream-take factorials 10))
