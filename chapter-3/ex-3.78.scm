164f2c45c217ee0f52ab4fec7db9b87c3d7e4472
yysaki <yysaki@yysaki.com>
2017-09-11 20:38:36 +0900

    Solve 3.78

---
+++ chapter-3/ex-3.78.scm
@@ -0,0 +1,21 @@
+(load "./3.5")
+
+(define (integral delayed-integrand initial-value dt)
+  (cons-stream initial-value
+               (let ((integrand (force delayed-integrand)))
+                 (if (stream-null? integrand)
+                   the-empty-stream
+                   (integral (delay (stream-cdr integrand))
+                             (+ (* dt (stream-car integrand))
+                                initial-value)
+                             dt)))))
+
+(define (solve-2nd a b y0 dy0 dt)
+  (define y (integral (delay dy) y0 dt))
+  (define dy (integral (delay ddy) dy0 dt))
+  (define ddy (add-streams (scale-stream dy a)
+                           (scale-stream y b)))
+  y)
+
+(print (stream-ref (solve-2nd 4 -4 1 2 0.001) 1000))
+
